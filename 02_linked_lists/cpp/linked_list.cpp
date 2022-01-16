93372eaa1f1a42d5d0c15972ff2396ce0f0545b0
Kai Hulme <kai@desktop.Home>
2022-01-16 23:16:15 +0000

    Linked list remove tail

--- 02_linked_lists/cpp/linked_list.cpp
+++ 02_linked_lists/cpp/linked_list.cpp
@@ -17,18 +17,34 @@ class Node {
 		}
 
 		void append(int x) {
-			
 			Node* new_node = new Node(x);
-			Node* cur_node = this;
-
-			while (cur_node->next != NULL) {
-				cur_node = cur_node->next;
+			Node* curr_node = this;
+			while (curr_node->next != NULL) {
+				curr_node = curr_node->next;
 			}
-
-			cur_node->next = new_node;
-
+			curr_node->next = new_node;
 		}
 			
+		void remove_tail() {
+			Node* curr_node = this;
+			Node* prev_node;
+			while (curr_node->next != NULL) {
+				prev_node = curr_node;
+				curr_node = curr_node->next;
+			}
+			prev_node->next = NULL;
+			delete(curr_node);
+		}
+
+		void print() {
+			Node* curr_node = this;
+			while (curr_node->next != NULL) {
+				cout << "[" << curr_node->data << "] -> ";
+				curr_node = curr_node->next;
+			}
+			cout << "[" << curr_node->data << "] -> NULL;" << endl;
+		}
+
 };
 
 
@@ -41,13 +57,11 @@ int main() {
 		linked_list->append(i);
 	}
 
-	Node* node = linked_list;
-	for (int i=0; i<n; i++) {
-    	cout << node->data << endl;
-		if (node->next != NULL) {
-			node = node->next;
-		}
-	}
+	linked_list->print();
+
+	linked_list->remove_tail();
+
+	linked_list->print();
 
 	return 0;
 }
