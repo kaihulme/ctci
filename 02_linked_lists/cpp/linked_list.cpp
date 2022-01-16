d175306274a7595ad0c5ee6b5fc1b60b85aaeeb3
Kai Hulme <kai@desktop.Home>
2022-01-16 22:50:19 +0000

    Linked list append

--- 02_linked_lists/cpp/linked_list.cpp
+++ 02_linked_lists/cpp/linked_list.cpp
@@ -12,17 +12,42 @@ class Node {
 		int data;
 	
 		Node(int x) {
+			this->next = NULL;
 			this->data = x;
-		}		
+		}
 
+		void append(int x) {
+			
+			Node* new_node = new Node(x);
+			Node* cur_node = this;
+
+			while (cur_node->next != NULL) {
+				cur_node = cur_node->next;
+			}
+
+			cur_node->next = new_node;
+
+		}
+			
 };
 
 
 int main() {
 
-	Node linked_list = Node(5);
+	Node* linked_list = new Node(0);
 
-    cout << linked_list.data << endl;
+	int n=10;
+	for (int i=1; i<n; i++) {
+		linked_list->append(i);
+	}
+
+	Node* node = linked_list;
+	for (int i=0; i<n; i++) {
+    	cout << node->data << endl;
+		if (node->next != NULL) {
+			node = node->next;
+		}
+	}
 
 	return 0;
 }
