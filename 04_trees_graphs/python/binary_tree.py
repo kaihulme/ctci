fd9b5a678c2b8b83852583d13a50f4101e2f1434
kaihulme <kaihulme97@gmail.com>
2022-01-29 14:37:46 +0000

    autopep8

--- 04_trees_graphs/python/binary_tree.py
+++ 04_trees_graphs/python/binary_tree.py
@@ -1,11 +1,11 @@
 
 class BinaryTree:
-    
+
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
-        
+
     def insert_left(self, x):
         if not self.left:
             self.left = BinaryTree(x)
@@ -13,7 +13,7 @@ class BinaryTree:
             new_left = BinaryTree(x)
             new_left.left = self.left
             self.left = new_left
-            
+
     def insert_right(self, x):
         if not self.right:
             self.right = BinaryTree(x)
@@ -21,28 +21,28 @@ class BinaryTree:
             new_right = BinaryTree(x)
             new_right.right = self.right
             self.right = new_right
-            
+
     def pre_order(self):
         print(self.val)
         if self.left:
             self.left.pre_order()
         if self.right:
             self.right.pre_order()
-            
+
     def in_order(self):
         if self.left:
             self.left.pre_order()
         print(self.val)
         if self.right:
             self.right.pre_order()
-            
+
     def post_order(self):
         if self.left:
             self.left.pre_order()
         if self.right:
             self.right.pre_order()
         print(self.val)
-    
+
 
 btree = BinaryTree(1)
 btree.insert_left(2)
@@ -53,4 +53,3 @@ btree.insert_right(4)
 # btree.pre_order()
 # btree.in_order()
 btree.post_order()
-
--- 04_trees_graphs/python/min_heap.py
+++ 04_trees_graphs/python/min_heap.py
@@ -1,25 +1,25 @@
 class MinHeap:
-    
+
     def __init__(self):
         self.heap = [0]
         self.size = 0
-        
+
     def print_heap(self):
         if self.size:
             print(self.heap[1:])
         else:
             print("Heap empty!")
-        
+
     def peep_min(self):
         if not self.size:
             raise IndexError("Cannot remove from empty heap!")
         return self.heap[1]
-    
+
     def insert(self, x):
         self.heap.append(x)
         self.size += 1
         self.percolate_up(self.size)
-        
+
     def remove_min(self):
         if not self.size:
             raise IndexError("Cannot remove from empty heap!")
@@ -29,7 +29,7 @@ class MinHeap:
         self.size -= 1
         self.percolate_down(1)
         return min_val
-        
+
     def percolate_up(self, i):
         while i // 2 > 0:
             if self.heap[i] < self.heap[i // 2]:
@@ -37,7 +37,7 @@ class MinHeap:
                 self.heap[i] = self.heap[i // 2]
                 self.heap[i // 2] = x
             i //= 2
-            
+
     def percolate_down(self, i):
         while i * 2 <= self.size:
             min_child = self.get_min_child(i)
@@ -46,21 +46,21 @@ class MinHeap:
                 self.heap[i] = self.heap[min_child]
                 self.heap[min_child] = x
             i = min_child
-            
+
     def get_min_child(self, i):
         if i * 2 + 1 > self.size:
             return i * 2
         if self.heap[i * 2] <= self.heap[i * 2 + 1]:
             return i * 2
         return i * 2 + 1
-        
+
     def heapify(self, xs):
         self.heap = [0] + xs
-        self.size = len(xs)        
+        self.size = len(xs)
         for i in range(self.size // 2, 0, -1):
             self.percolate_down(i)
-            
-            
+
+
 min_heap = MinHeap()
 
 for x in range(5, 0, -1):
